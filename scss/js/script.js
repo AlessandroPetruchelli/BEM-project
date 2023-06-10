0335fbb7a730d583b2974d2823b40b66dc7e9eb8
AlessandroPetruchelli <sanya_vdv777@mail.ru>
2023-06-10 20:26:21 +0300

    first commit

---
+++ scss/js/script.js
@@ -0,0 +1,8 @@
+/* let age = prompt('Сколько тебе лет?', 100 );
+alert(`Тебе ${age} лет!`); */
+let age = prompt('Введите первое число');
+let age2 = prompt('Введите второе число');
+alert(Number(age) + Number(age2));
+
+
+
